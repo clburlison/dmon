972d58c949bbd450b2df1923a648698e384586dd
Clayton Burlison <git@clburlison.com>
2023-04-06 18:15:00 -0500

    Spaghetti (#6)
    * feat: working monitor + update
    
    * docs: Update README
    
    * feat: Version checks
    
    * feat: Helper enable/disable scripts
    
    * feat: Resolve SSL traffic issue

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 A monitor solution for jailbroken iOS devices. The core goal of this project is to make sure a specific iOS application is constantly running without needed to use Single App Mode (SAM) or Guided Access Mode (GAM).
 
-A `./bin/setup` script is included to help with initial configuration of a jailbroken device.
+A script, `./bin/setup`, is included to help with initial configuration of a jailbroken device.
 
 Lastly, in the future I would like to include a way to handle updates for the various components.
 
@@ -17,44 +17,73 @@ If you only care about the monitoring component from this repo you can grab the 
 1. Run `dpkg -i com.github.clburlison.dmon-XXX.deb`
 1. The LaunchDaemon service will now monitor to make sure all components are properly running
 
+## Prerequisites
+
+- A Mac
+- A jailbroken iPhone that is supervised
+- Apple Command Line Tools (`xcode-select --install`)
+- imobiledevice tools (`brew install libimobiledevice`)
+- Optional but **highly recommend** creating a ssh keypair
+- Setup your ssh config entry. This makes your life much easier as ssh sessions can be remembered.
+
+  ```sh
+  cat ~/.ssh/config
+  Host iphone
+    HostName localhost
+    User root
+    Port 2222
+    StrictHostKeyChecking no
+
+  Host *
+    ControlMaster auto
+    ControlPath /tmp/%r@%h:%p
+    ControlPersist 1800
+  ```
+
 ## Getting started
 
 It is assumed you know your way around a command line. All commands are run on your computer connected to a single iOS device. While it is possible to do some of this manually on a jailbroken iOS device that is pron to human error.
 
 1. Grab a valid iOS 14+ device and jailbreak it: https://ios.cfw.guide/get-started/select-iphone/
-1. Clone this git repo
+1. Clone this git repo.
 
    ```sh
    git clone https://github.com/clburlison/dmon
    ```
 
-1. Change directory into the freshly cloned repo
+1. Change directory into the freshly cloned repo.
 
    ```sh
    cd dmon
    ```
 
-1. Create a `config.json` at the root of this repo with the correct values
+1. Create a `config.json` at the root of this repo with the correct values.
+
+   Make sure to remove all `// comments` before saving. They are not valid json!
 
    ```json
    {
-     "api_key": "YOUR_GC_API_KEY",
-     "device_configuration_manager_url": "https://my_awesome_DCM_url"
+     "api_key": "YOUR_API_KEY",
+     "device_configuration_manager_url": "https://YOUR_AWESOME_DCM_URL",
+     "dmon_url": "https://YOUR_URL:PORT/path/", // Url to download update files from
+     "dmon_username": "username", // Basic Auth username. Leave empty if not used
+     "dmon_password": "password" // Basic Auth password. Leave empty if not used
    }
    ```
 
-1. Download any extra .deb files you want installed into the `./debs/` directory. These are installed based on file name IE 01_foobar.deb, 02_curl.deb, etc.
-
-   > Substitute is installed as part of the `setup` script
+1. Download any extra .deb files you want installed into the `./debs/` directory.
 
    debs to include:
 
+   - https://apt.bingner.com/debs/1443.00/com.ex.substitute_2.3.1_iphoneos-arm.deb
+   - https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6005_iphoneos-arm.deb
    - https://repo.spooferpro.com/debs/com.spooferpro.kernbypass_1.1.0_iphoneos-arm64.deb
-   - Potentially any paid/private debs. nudge, nudge, wink, wink
+   - https://github.com/clburlison/dmon/releases
    - (Optional) https://cydia.akemi.ai/debs/nodelete-ai.akemi.appsyncunified.deb
    - (Optional) https://cydia.akemi.ai/debs/nodelete-ai.akemi.appinst.deb
+   - **Potentially any paid/private debs. nudge, nudge, wink, wink**
 
-1. Grab a copy of Pokemon Go via [majd/ipatool](https://github.com/majd/ipatool)
+1. Grab a copy of Pokemon Go via [majd/ipatool](https://github.com/majd/ipatool).
 
    ```sh
    brew tap majd/repo
@@ -63,8 +92,8 @@ It is assumed you know your way around a command line. All commands are run on y
    ipatool download --purchase -b com.nianticlabs.pokemongo -o pogo.ipa
    ```
 
-1. Connect your iOS device to your computer via USB
-1. Open Terminal and run (remember to only have one phone connected)
+1. Connect your iOS device to your computer via USB.
+1. Open Terminal and run (remember to only have one phone connected).
 
    ```sh
    # Alteratively you can pass -u <device-uuid> if multiple phones are connected
@@ -75,13 +104,14 @@ It is assumed you know your way around a command line. All commands are run on y
 
    ```sh
    ssh root@localhost -p 2222 # default password is 'alpine'
+   # Now disconnect with: Control + d
    ```
 
-1. In a third terminal window run:
+1. Now run:
 
    ```sh
    ./bin/setup
-   # If you want to setup passwordless ssh then pass the argument with the path to your key
+   # If you want to setup passwordless ssh then pass the argument with the path to your public key
    ./bin/setup -s ~/.ssh/main.pub
    ```
 
@@ -97,9 +127,51 @@ Bonus items that are out of scope for this project.
 
 - All testing has been completed with iOS 15 using palera1n
 - Only confirmed on older A9 processors aka iPhone SE first gen
-- DEB Package is build on macOS Ventura
+- DEB Package is built on macOS Ventura
+
+## Commonly asked questions
+
+### Why didn't you use Theos to build the deb?
+
+I was expecting to add a few external compiled binaries and didn't want to read a ton of documentation. Things changed and now I'm too lazy to rewrite.
+
+### How can I stop it?!?!
+
+1. Close Pokemon Go on the phone
+2. ssh into the phone & unload the launch daemon
+
+   ```sh
+   ssh iphone
+   /usr/bin/launchctl unload /Library/LaunchDaemons/com.github.clburlison.dmon.plist
+   ```
+
+### How do I setup the webserver?
+
+It is a flat structure. You can use nginx, apache, caddy, python, node, etc. Your files should be named like this:
+
+```sh
+top_level_folder
+├── gc.deb
+├── pogo.ipa
+└── version.txt
+```
+
+Then in your config point `dmon_url` to `http://HOSTNAME:PORT/top_level_folder`.
+
+### Why did you reuse the existing `config.json`?
+
+This isn't a pure solution. I am lazy. Now bugger off.
+
+### Why didn't you include the debs I need?
+
+I don't have the original authors permissions to upload their files.
+
+### Why is my https url not working?
+
+We are using the stock CA Certificates installed as part of the iOS jailbreak. The Procursus Team placed files in `/usr/lib/ssl/cacert.pem` and I figured it would be safe to keep using them. Those root certs might have expired and need an update.
 
 ## References
 
 - [dm.pl](https://github.com/theos/dm.pl)
+- [theos](https://theos.dev)
 - [appknox/Open](https://github.com/appknox/Open) which was originally from [conradev/Open](https://github.com/conradev/Open)
---
+++ tools/dmon.m
@@ -0,0 +1,272 @@
+#import <Foundation/Foundation.h>
+#include <CoreFoundation/CoreFoundation.h>
+#import <SpringBoardServices/SpringBoardServices.h>
+#import <dlfcn.h>
+#include <curl/curl.h>
+
+
+NSString * getFrontMostApplication() {
+    mach_port_t p = SBSSpringBoardServerPort();
+    char frontmostAppS[256];
+    memset(frontmostAppS, 0, sizeof(frontmostAppS));
+    SBFrontmostApplicationDisplayIdentifier(p, frontmostAppS);
+    
+    NSString * frontmostApp = [NSString stringWithFormat:@"%s", frontmostAppS];
+    NSLog(@"dmon: Frontmost app is: %@", frontmostApp);
+    return (frontmostApp);
+}
+
+NSString * installedPogoVersion(void) {
+    NSLog(@"dmon: Finding Pogo version...");
+    char bundlePath[150]; // This should never be longer than 100 but we added a buffer
+    FILE *pogo_cmd = popen("find /var/containers/Bundle/Application/ -type d -name 'PokmonGO.app'", "r");
+    if (pogo_cmd == NULL) {
+        NSLog(@"dmon: Unable to find PokemonGo on device");
+        return nil;
+    }
+
+    fgets(bundlePath, sizeof(bundlePath), pogo_cmd);
+    pclose(pogo_cmd);
+    bundlePath[strcspn(bundlePath, "\n")] = '\0'; // Remove newline character
+    sprintf(bundlePath, "%s/Info.plist", bundlePath);
+
+    NSString *path = [[NSString alloc] initWithUTF8String:bundlePath];
+    NSDictionary *infoDict = [[NSDictionary alloc] initWithContentsOfFile:path];
+    NSString *versionString = [infoDict objectForKey:@"CFBundleShortVersionString"];
+    NSLog(@"dmon: Installed version of Pokemon Go: %@", versionString);
+    return versionString;
+}
+
+NSString * installedGCVersion(void) {
+    // Can't use this really clean method since this library isn't versioned. Big Sad!
+    // NSBundle *goCheatsBundle = [NSBundle bundleWithPath:@"/Library/MobileSubstrate/DynamicLibraries/libgocheats.dylib"];
+    // NSLog(@"dmon: bundle info is: %@", goCheatsBundle);
+    // NSString *goCheatsVersion = [goCheatsBundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    // NSLog(@"dmon: Version: %@", goCheatsVersion);
+    // return goCheatsVersion;
+
+    NSLog(@"dmon: Finding GC version...");
+    FILE *gc_cmd = popen("apt list 2>/dev/null | grep -i gocheats | cut -d ' ' -f 2", "r");
+    if (gc_cmd == NULL) {
+        NSLog(@"dmon: Unable to find GC version");
+        return nil;
+    }
+
+    char version[256];
+    int bytesRead = fread(version, sizeof(char), 255, gc_cmd);
+    version[bytesRead] = '\0';
+    pclose(gc_cmd);
+    version[strcspn(version, "\n")] = '\0'; // Remove newline character
+
+    NSLog(@"dmon: Installed version of GC: %s", version);
+    return [[NSString alloc] initWithUTF8String:version];
+}
+
+NSDictionary * parseConfig(void) {
+    NSString *filePath = @"/var/mobile/Application Support/GoCheats/config.json";
+    NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+    NSError *error;
+    NSData *fileData = [NSData dataWithContentsOfURL:fileURL options:NSDataReadingMappedIfSafe error:&error];
+
+    if (fileData) {
+        NSError *jsonError;
+        NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:fileData options:NSJSONReadingMutableContainers error:&jsonError];
+        
+        if (jsonObject) {
+            // Successfully parsed the JSON data
+            return jsonObject;
+        } else {
+            // Failed to parse the JSON data
+            NSLog(@"dmon: Error parsing JSON: %@", jsonError);
+        }
+    } else {
+        // Failed to read the file data
+        NSLog(@"dmon: Error reading file: %@", error);
+    }
+    return nil;
+}
+
+NSMutableDictionary * parseKeyValueFileAtPath(NSString *filePath) {
+    NSMutableDictionary *resultDict = [NSMutableDictionary dictionary];
+    
+    NSError *error = nil;
+    NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error];
+    if (error) {
+        NSLog(@"dmon: Error reading file: %@", error);
+        return nil;
+    }
+    
+    NSArray *lines = [fileContents componentsSeparatedByString:@"\n"];
+    for (NSString *line in lines) {
+        NSArray *parts = [line componentsSeparatedByString:@": "];
+        if (parts.count == 2) {
+            NSString *key = parts[0];
+            NSString *value = parts[1];
+            [resultDict setObject:value forKey:key];
+        }
+    }
+    
+    return resultDict;
+}
+
+int killall(NSString *appName) {
+    NSLog(@"dmon: Stopping appName: %@", appName);
+    int stop_pid;
+    char command[100]; // Make it large enough.
+    sprintf(command, "killall %s 2>/dev/null", [appName UTF8String]);
+    FILE *stop_pid_cmd = popen(command, "r");
+    fscanf(stop_pid_cmd, "%d", &stop_pid);
+    pclose(stop_pid_cmd);
+    return stop_pid;
+}
+
+int installIpa(NSString *filePath) {
+    NSLog(@"dmon: Installing: %@...", filePath);
+    int results;
+    char command[100];
+    sprintf(command, "appinst %s", [filePath UTF8String]);
+    FILE *install_ipa_cmd = popen(command, "r");
+    fscanf(install_ipa_cmd, "%d", &results);
+    pclose(install_ipa_cmd);
+    return results;
+}
+
+int installDeb(NSString *filePath) {
+    NSLog(@"dmon: Installing: %@...", filePath);
+    int ext_code;
+    char deb_command[100];
+    sprintf(deb_command, "dpkg -i %s", [filePath UTF8String]);
+    FILE *install_deb_cmd = popen(deb_command, "r");
+    char output[100];
+    while (fgets(output, sizeof(output), install_deb_cmd) != NULL) {
+        NSLog(@"dmon: dpkg output: %s", output);
+    }
+    ext_code = pclose(install_deb_cmd);
+    NSLog(@"dmon: Results for %@ are: %d", filePath, ext_code);
+    return ext_code;
+}
+
+int downloadFile(NSString *url, NSString *userpass, NSString *outfile) {
+    CURL *curl;
+    CURLcode res = -1;
+    FILE *fp;
+    curl = curl_easy_init();
+
+    if (curl) {
+        // Set the URL
+        curl_easy_setopt(curl, CURLOPT_URL, [url UTF8String]);
+        
+        // Set the authentication headers
+        curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_easy_setopt(curl, CURLOPT_USERPWD, [userpass UTF8String]);
+        
+        // Set the output file
+        fp = fopen([outfile UTF8String], "wb");
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+        // Set the path to the cacerts file
+        curl_easy_setopt(curl, CURLOPT_CAINFO, "/usr/lib/ssl/cacert.pem");
+
+        // Perform the request
+        res = curl_easy_perform(curl);
+        NSLog(@"dmon: Curl return code for %@: %u", url, res);
+
+        // Clean up
+        fclose(fp);
+        curl_easy_cleanup(curl);
+    }
+    return res;
+}
+
+void update(NSDictionary *config) {
+    NSString *versionFile = @"version.txt";
+    NSString *pogo_ipa = @"pogo.ipa";
+    NSString *gc_deb = @"gc.deb";
+    NSString *pogoVersion = installedPogoVersion();
+    NSString *gcVersion = installedGCVersion();
+
+    // Strip trailing forward slashes to make things consistent for users
+    NSString *url = [config[@"dmon_url"] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"/"]];
+    NSLog(@"dmon: Update URL is: %@", url);
+    downloadFile(
+        [NSString stringWithFormat:@"%@/%@", url, versionFile],
+        [NSString stringWithFormat:@"%@:%@", config[@"dmon_username"], config[@"dmon_password"]],
+        versionFile
+    );
+
+    // Parse the config map style version.txt to NSDictionary
+    NSMutableDictionary *parsedVersion = parseKeyValueFileAtPath(versionFile);
+
+    // Update Pogo if needed
+    if (![pogoVersion isEqualToString:parsedVersion[@"pogo"]]) {
+        NSLog(@"dmon: Pogo version mismatch. Have %@. Need %@", pogoVersion, parsedVersion[@"pogo"]);
+        int pogoDownload = downloadFile(
+            [NSString stringWithFormat:@"%@/%@", url, pogo_ipa],
+            [NSString stringWithFormat:@"%@:%@", config[@"dmon_username"], config[@"dmon_password"]],
+            pogo_ipa
+        );
+        // TODO: Do we need a stricter validation?
+        if (pogoDownload == 0) {
+            installIpa(pogo_ipa);
+        }
+    }
+
+    // Update GC if needed
+    if (![gcVersion isEqualToString:parsedVersion[@"gc"]]) {
+        NSLog(@"dmon: GC version mismatch. Have %@. Need %@", gcVersion, parsedVersion[@"gc"]);
+        int gcDownload = downloadFile(
+            [NSString stringWithFormat:@"%@/%@", url, gc_deb],
+            [NSString stringWithFormat:@"%@:%@", config[@"dmon_username"], config[@"dmon_password"]],
+            gc_deb
+        );
+        // TODO: Do we need a stricter validation?
+        if (gcDownload == 0) {
+            installDeb(gc_deb);
+        }
+    }
+}
+
+void monitor(void) {
+    NSString *currentApp = getFrontMostApplication();
+    if (![currentApp isEqualToString:@"com.nianticlabs.pokemongo"]) {
+        NSLog(@"dmon: Restarting Kernbypass...");
+        killall(@"/usr/bin/kernbypass");
+        NSLog(@"dmon: Force stopping Pogo...");
+        killall(@"pokemongo");
+        sleep(2);
+        NSLog(@"dmon: Pogo not running. Launch it...");
+        // Launch Pogo
+        void* sbServices = dlopen("/System/Library/PrivateFrameworks/SpringBoardServices.framework/SpringBoardServices", RTLD_LAZY);
+        int (*SBSLaunchApplicationWithIdentifier)(CFStringRef identifier, Boolean suspended) = dlsym(sbServices, "SBSLaunchApplicationWithIdentifier");
+        NSString *bundleString=[NSString stringWithUTF8String:"com.nianticlabs.pokemongo"];
+        int result = SBSLaunchApplicationWithIdentifier((__bridge CFStringRef)bundleString, NO);
+        NSLog(@"dmon: Launch Pogo: %d", result);
+        dlclose(sbServices);
+    }
+}
+
+int main(void) {
+    NSLog(@"dmon: Starting...");
+
+    // Start loop
+    int i = 0;
+    while (1) {
+        // Only call at the start of loop
+        if (i == 0) {
+            NSDictionary *config = parseConfig();
+            // NSLog(@"dmon: Full config: %@", config);
+            update(config);
+        }
+
+        // Call this function every loop
+        monitor();
+
+        // Restart loop on the 30th iteration
+        if (++i == 30) {
+            i = 0;
+        }
+
+        sleep(30);
+    }
+    return 0;
+}
